e2f84b823462b6761a97b2fe9732d7be2bd8ebac
Lidia Morozova <lgliducik@gmail.com>
2012-10-21 22:08:43 +0400

    upgrate 5 problem

--- ex5.py
+++ ex5.py
@@ -1,4 +1,8 @@
-l = input()
+import sys
+import itertools        
+        
+
+#l = input()
 f = open("text.txt",'w')
 alph = {
     'UUU': 'F',
@@ -6,7 +10,7 @@ alph = {
     'AUU': 'I',
     'GUU': 'V',
     'UUC': 'F','CUC' :'L','AUC': 'I',
-'GUC': 'V','UUA': 'L','CUA' :'L','AUA' :'I','GUA' :'V', 'UUG' :'L','CUG' :'L',      
+'GUC': 'V','UUA': 'L','CUA' :'L','AUA' :'I','GUA' :'V', 'UUG' :'L','CUG' :'L',
 'AUG': 'M','GUG': 'V','UCU' :'S','CCU' :'P','ACU': 'T','GCU' :'A','UCC' :'S',
 'CCC': 'P','ACC': 'T','GCC': 'A','UCA' :'S','CCA': 'P','ACA': 'T','GCA': 'A',
 'UCG' :'S', 'CCG': 'P', 'ACG': 'T','GCG': 'A','UAU' :'Y', 'CAU' :'H','AAU': 'N',
@@ -15,21 +19,17 @@ alph = {
 'CGU': 'R','AGU': 'S','GGU': 'G','UGC' :'C','CGC': 'R','AGC': 'S','GGC': 'G',
 'UGA' :'Stop', 'CGA': 'R','AGA':'R','GGA': 'G','UGG' :'W','CGG': 'R','AGG': 'R','GGG': 'G' }
 
-
-i = 0
-newl = []
-for element in l:
-    if i<len(l)/3:
-
-        newstr = l[ i * 3 : ( i + 1 ) * 3 ]
-        s = alph[newstr]
-        newl.append(s)
-    i = i + 1
-string = ''
-ii = 0
-for element in newl:
-    if newl[ii] != 'Stop':
-        string = string + element
-        ii = ii + 1
-print string
-f.write(string)
+for l in open(sys.argv[1]):
+        i = 0
+        newl = []
+        for i in range(0, len(l), 3):
+            newstr = l[i: i + 3]
+            s = alph[newstr]
+            newl.append(s)
+                        
+                        
+        
+        
+        iter = itertools.takewhile(lambda x: x != 'Stop', newl)
+        string = "".join(iter)
+        f.write(string)
